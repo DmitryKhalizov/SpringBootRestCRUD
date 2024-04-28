548ead6a80af431b130286b3120aba222dfe1ca3
DmitryKhalizov <khalizov@hotmail.com>
2024-04-28 16:03:11 +0200

    Commit #2. Initial commit of the project

---
+++ src/main/java/se/dmitrykhalizov/springbootrestcrud/dao/EmployeeDAO.java
@@ -0,0 +1,19 @@
+package se.dmitrykhalizov.springbootrestcrud.dao;
+
+import se.dmitrykhalizov.springbootrestcrud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+
+
+}
